be36e63fc876fea5faed07cf17cc9132a12f9a25
Wodman <w0dm4n@hotmail.fr>
2017-07-03 02:40:06 +0200

    Windows entry point and default, with conhost hided

--- srcs/main.c
+++ srcs/main.c
@@ -11,9 +11,28 @@
 /* ************************************************************************** */
 
 #include "all.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+/*int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    PSTR szCmdParam, int iCmdShow)
+{
+    FreeConsole();
+    for (;;) {
+        printf("Hello !\n");
+        //action here
+    }
+    return 0;
+}*/
 
 int     main(int argc, char **argv)
 {
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow( hWnd, SW_MINIMIZE );
+    ShowWindow( hWnd, SW_HIDE );
+    FreeConsole();
     setlocale(LC_CTYPE, "");
     wprintf(L"®π® Hello world ®π®\n");
     while (true)
